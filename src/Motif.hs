68a9e490523eb85d209b052a6d61288f1ec8cbf8
James Sully <sullyj3@gmail.com>
2021-11-19 14:20:33 +1100

    Initial

---
+++ src/Motif.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE NumericUnderscores #-}
+module Motif
+    ( sleep
+    , module Flow
+    ) where
+
+import Control.Concurrent (threadDelay)
+import Flow
+import Data.Foldable
+
+sleep :: Double -> IO ()
+sleep seconds = threadDelay . round $ (seconds * 1_000_000)
+
+{-# specialise count :: (a -> Bool) -> [a] -> Int #-}
+count :: Foldable t => (a -> Bool) -> t a -> Int
+count p = foldl' go 0
+  where go n x | p x = n+1
+               | otherwise = n
